f011ee42db6775b06c9d1ab5fb830393519485b8
PimaUA <pimaua87@gmail.com>
2022-07-31 15:50:49 +0300

    changes for words with hyphen

--- src/part1/Count.java
+++ src/part1/Count.java
@@ -9,7 +9,7 @@ public class Count {
         //checking if each element of array is word
         int numberOfWords=0;
         for(String element:strArray){
-      if(element.matches("(\\p{P}?([a-zA-Z]+)\\p{P}?)")){
+      if(element.matches("(\\p{P}?([a-zA-Z\\-]+)\\p{P}?)")){
           numberOfWords++;
       }
         }
@@ -18,7 +18,7 @@ public class Count {
 //method main
     public static void main(String[] args) {
         Count count = new Count();
-        int numberOfWords=count.wordsCount(" .Hello,   my other   23 worlds! ");
+        int numberOfWords=count.wordsCount(" .Hello -hello,   my other   23 worlds! ");
         System.out.println(numberOfWords);
     }
 }
